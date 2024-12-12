68bbf915a1f88f1e6223f67f1df3d7b45b2f4118
Jhoyan <jhoyanpdv@hotmail.com>
2024-12-11 20:21:28 -0400

    feat: segunda atividade de introdução a laços de repetição

---
+++ 1LacosDeRepeticaoExercicio02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Pipes;
+using static System.Console;
+using static System.Convert;
+
+double altPedro = 1.5;
+double taxaCresPedro = 0.02;
+double altLucas = 1.1;
+double taxaCresLucas = 0.03;
+int anos = 0;
+int anosIgual = 0;
+
+while (altLucas <= altPedro)
+{
+    altLucas += taxaCresLucas;
+    altPedro += taxaCresPedro;
+    if (anosIgual == 0 && altLucas >= altPedro)
+    {
+        anosIgual = anos;
+        anos++;
+    } else { anos++; }
+}
+WriteLine($"Levará {anosIgual} anos até que Lucas e Pedro tenham a mesma altura e {anos} anos até que Lucas ultrapasse a altura de Pedro.");
