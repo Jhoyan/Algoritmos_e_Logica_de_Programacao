e0f4e04763306ad1770b89c5cb1b03a27e457382
Jhoyan <jhoyanpdv@hotmail.com>
2024-12-11 20:23:01 -0400

    feat: terceira atividade de introdução a laços de repetição

---
+++ 1LacosDeRepeticaoExercicio03/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int n  = 0;
+int m = 1;
+
+WriteLine("Digite o número da tabuada: ");
+n = ToInt32(ReadLine());
+
+for (int j = 0; j < n; j++)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        WriteLine($"{m} X {i} = {m * i}");
+    }
+    m++;
+}
