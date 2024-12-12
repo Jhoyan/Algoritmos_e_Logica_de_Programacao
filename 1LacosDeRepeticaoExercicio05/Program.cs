d3b880e44a30a7e8922f87a69e8220e75693d10d
Jhoyan <jhoyanpdv@hotmail.com>
2024-12-11 20:24:07 -0400

    feat: quinta atividade de introdução a laços de repetição

---
+++ 1LacosDeRepeticaoExercicio05/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int fibo = 0;
+int atual = 1;
+int anterior = 0;
+int n  = 0;
+
+WriteLine("Escreva quantos números da sequência de Fibonacci deseja saber: ");
+n = ToInt32(ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    fibo = anterior + atual;
+    WriteLine(fibo);
+    anterior = atual;
+    atual = fibo;
+}
