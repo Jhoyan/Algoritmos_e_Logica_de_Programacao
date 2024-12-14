78c9d281fd0346e2e153b7e691133d6cc969cb69
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-14 16:30:04 -0400

    feat: fifth execise - getting better at loops

---
+++ 2LacosDeRepeticaoExercicio05/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int qtd = 0;
+int positivos = 0;
+int negativos = 0;
+double soma = 0;
+double media = 0;
+double numero = 0;
+
+do
+{
+    Write("Digite um número (0 para finalizar): ");
+    numero = ToDouble(ReadLine());
+
+    soma += numero;
+    qtd++;
+
+    if (numero > 0)
+    {
+        positivos++;
+    }
+    else if (numero < 0)
+    {
+        negativos++;
+    }
+} while (numero != 0);
+
+media = soma / (qtd-1);
+WriteLine($"Média aritmética: {media}");
+WriteLine($"Quantidade de valores positivos: {positivos}");
+WriteLine($"Quantidade de valores negativos: {negativos}");
