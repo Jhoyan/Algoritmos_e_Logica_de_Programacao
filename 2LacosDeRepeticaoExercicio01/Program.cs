3ff5f123bb27017bd01f4ecfcc9913bc0677ad6a
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-13 22:02:12 -0400

    feat: first execise - getting better at loops

---
+++ 2LacosDeRepeticaoExercicio01/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int n = 0;
+int qtd = 0;
+
+for (int i = 0; i < 30; i++)
+{
+    WriteLine("Digite um número: ");
+    n = ToInt32(ReadLine());
+    if (n > 100)
+    {
+        qtd++;
+    }
+}
+
+WriteLine($"A quantidade de números digitados maiores que 100 é de: {qtd}");
