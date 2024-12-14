8c185161b5d0636947569400deae29dcdfa68ce8
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-13 22:09:12 -0400

    feat: second execise - getting better at loops

---
+++ 2LacosDeRepeticaoExercicio02/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int n = 0;
+
+for(int i = 0; i < 10; i++)
+{
+    WriteLine("Digite um número: ");
+    n = ToInt32(ReadLine());
+    if(n % 3 == 0)
+    {
+        WriteLine($"O número {n} é divisível por 3");
+    }
+}
