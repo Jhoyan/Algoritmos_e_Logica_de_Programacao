5bf651cb3bac628542d69d034b0b6ce4ebba4225
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-14 16:31:06 -0400

    feat: sixth execise - getting better at loops

---
+++ 2LacosDeRepeticaoExercicio06/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int negativos = 0;
+int numero;
+
+do
+{
+    Write("Digite um número inteiro (Digite 0 para finalizra): ");
+    numero = ToInt32(ReadLine());
+
+    if (numero < 0)
+    {
+        negativos += numero;
+    }
+
+} while (numero != 0);
+
+WriteLine($"A soma dos números negativos é {negativos}");
