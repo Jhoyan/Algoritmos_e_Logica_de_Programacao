6473aa9105abc4de866ae698115541e140e47dde
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-14 16:28:04 -0400

    feat: third execise - getting better at loops

---
+++ 2LacosDeRepeticaoExercicio03/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int n = 0;
+double soma = 0;
+double qtd = 0;
+double media = 0;
+
+do
+{
+    WriteLine("Digite um número: ");
+    n = ToInt32(ReadLine());
+    soma += n;
+    qtd++;
+} while (n != 0);
+
+media = soma / (qtd-1);
+WriteLine($"A média dos {qtd-1} valores lidos é de {media}");
