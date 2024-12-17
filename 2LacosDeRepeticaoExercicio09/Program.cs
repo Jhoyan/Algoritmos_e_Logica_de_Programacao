db7ccdb6909556a0b8a9c2a4b7af641fa4a51fa0
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-17 12:39:06 -0400

    feat: ninth execise - getting better at loops

---
+++ 2LacosDeRepeticaoExercicio09/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using static System.Convert;
+
+string nomeProduto;
+double precoCusto, novoPreco;
+int continuar;
+
+do
+{
+    WriteLine("Digite o nome do produto: ");
+    nomeProduto = ReadLine();
+
+    WriteLine("Digite o preço de custo do produto: ");
+    precoCusto = ToDouble(ReadLine());
+
+    novoPreco = precoCusto * 1.15;
+
+    WriteLine($"O produto {nomeProduto} terá um preço novo de {novoPreco:F2}\nDigite 0 para sair ou outro número para continuar: ");
+    continuar = ToInt32(ReadLine());
+}
+while (continuar != 0);
