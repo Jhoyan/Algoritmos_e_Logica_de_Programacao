8495b4733589c3b9b869640dbfbc207b3555c404
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-16 22:02:16 -0400

    feat: seventh execise - getting better at loops

---
+++ 2LacosDeRepeticaoExercicio07/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using static System.Convert;
+
+string nome;
+int salario;
+int continuar = 0;
+
+do
+{
+    WriteLine("Digite o nome do funcionário: ");
+    nome = ReadLine();
+
+    WriteLine("Digite o salário do funcionário: ");
+    salario = ToInt32(ReadLine());
+
+    if (salario >= 1000)
+    {
+        WriteLine($"Funcionário: {nome}, Salário: R$ {salario}");
+    }
+
+    WriteLine("Deseja digitar outro funcionário? (Digite 0 para encerrar): ");
+    continuar = ToInt32(ReadLine());
+
+} while (continuar != 0);
