a84a4d0d669007f855435f395e98e55da66d51f7
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-17 13:14:59 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio 1/Program.cs
@@ -0,0 +1,10 @@
+﻿double num1, num2, soma;
+
+Console.WriteLine("Digite o primeiro número: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o segundo número: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+soma = num1 + num2;
+
+Console.WriteLine($"O valor da soma dos dois valores é {soma}.");
