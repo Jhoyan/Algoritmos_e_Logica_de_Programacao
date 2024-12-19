3f99bcf67fa5d827073e43780897b92a746767ea
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-18 21:55:05 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.AccessControl;
+
+double altura, comprimento, largura, volume;
+
+Console.WriteLine("Qual a altura do retângulo em cm? ");
+altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Qual o comprimento do retângulo em cm? ");
+comprimento = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Qual a largura do retângulo em cm? ");
+largura = Convert.ToDouble(Console.ReadLine());
+
+volume = altura * comprimento * largura;
+Console.WriteLine($"O volume do retângulo é de {volume}cm cúbicos");
