f40967d1bba607c5c18d76c694fe8e4f1bf891d0
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-18 21:42:26 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,9 @@
+﻿double valor, quadrado, cubo;
+
+Console.WriteLine("Digite um valor: ");
+valor = Convert.ToDouble(Console.ReadLine());
+
+quadrado = valor * valor;
+cubo = valor * valor * valor;
+
+Console.WriteLine($"O quadrado de {valor} é igual a {quadrado}, enquanto o cubo de {valor} é igual a {cubo}.");
