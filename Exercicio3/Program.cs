3c0227401a317fa00c6e702687edbec96d523879
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-18 21:41:45 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,7 @@
+﻿double tempCelcius, tempFahren;
+
+Console.WriteLine("Qual a temperatura em graus Celcius? ");
+tempCelcius = Convert.ToDouble(Console.ReadLine());
+
+tempFahren = (9 * tempCelcius + 160) / 5;
+Console.WriteLine($"A temperatura em Fahrenheit é de {tempFahren}");
