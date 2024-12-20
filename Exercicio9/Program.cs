419d9b4ea60a6f2c6827f7394bf71daed389497e
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-19 23:12:12 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio9/Program.cs
@@ -0,0 +1,15 @@
+﻿int valor1;
+int valor2;
+int quocienteInteiro;
+int resto;
+
+Console.WriteLine("Qual o numerador?");
+valor1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Qual o denominador?");
+valor2 = Convert.ToInt32(Console.ReadLine());
+
+quocienteInteiro = valor1 / valor2;
+Console.WriteLine($"O valor do quociente inteiro é de {quocienteInteiro}");
+resto = valor1 % valor2;
+Console.WriteLine($"O valor do resto é de {resto}");
