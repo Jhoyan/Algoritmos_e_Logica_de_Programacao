ceb11c293d3a474141c31faa192701ed85f239d4
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-19 23:10:02 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,12 @@
+﻿int valor1;
+int valor2;
+int resultado;
+
+Console.WriteLine("Qual o primeiro valor? ");
+valor1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Qual o segundo valor? ");
+valor2 = Convert.ToInt32(Console.ReadLine());
+
+resultado = (valor1*valor1) + (valor2*valor2);
+Console.WriteLine($"O resultado da soma dos quadrados é de {resultado}");
