f8ec9c493b3c8abf9a62dc2123ae3520fce72efb
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-19 23:11:16 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,8 @@
+﻿double velocidadekmh;
+double velocidadems;
+
+Console.WriteLine("Qual a velocidade em KM/H? ");
+velocidadekmh = Convert.ToDouble(Console.ReadLine());
+
+velocidadems = velocidadekmh / 3.6;
+Console.WriteLine($"A velocidade em m/s é de {velocidadems}.");
