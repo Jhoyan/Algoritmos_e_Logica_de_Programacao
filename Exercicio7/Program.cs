22a7cc2eccf14563ed8517ccf0e3260517c7ddea
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2024-12-19 23:10:38 -0400

    feat: introduction to programming
    this is one of my first exercises while learning the basic of programming

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,9 @@
+﻿string numA, numB, ordemInversa;
+
+Console.WriteLine("Qual o primeiro valor? ");
+numA = Console.ReadLine();
+Console.WriteLine("Qual o segundo valor? ");
+numB = Console.ReadLine();
+ordemInversa = numB + numA;
+
+Console.WriteLine($"A ordem inversa dos valores é {ordemInversa}");
