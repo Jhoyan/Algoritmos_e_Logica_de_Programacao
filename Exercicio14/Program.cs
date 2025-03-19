1e728f8a83be5c457877de7f0471ce9a1125d4cf
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2025-03-18 23:51:25 -0400

    feat: introduction to programming

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console; using static System.Convert;
+
+int anos = 0;
+double diasVividos = 0.0;
+
+WriteLine("Qual sua idade? ");
+anos = ToInt32(ReadLine());
+
+diasVividos = (anos * 12) * 30;
+WriteLine($"Você tinha vivido um total de {diasVividos} dias quando completou {anos} anos.");
