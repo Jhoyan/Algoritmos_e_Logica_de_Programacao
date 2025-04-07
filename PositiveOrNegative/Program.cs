0b620f62979a7654a347e5804b7ddc1e6d647ba7
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2025-04-07 16:47:09 -0400

    feat: positive or negative
    first semester: I made this code to identify if a number is positive, negative or zero. There's a while loop running until the user types an invalid value (something other than a number), then the terminal asks if the user wants to continue and they need to choose between "sim" (yes) and "nao" (no).

---
+++ PositiveOrNegative/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design;
+
+while (true)
+{
+    string input;
+    int numero;
+    string condicao;
+    Console.WriteLine("Digite um número inteiro");
+    input = Console.ReadLine();
+    if (int.TryParse(input, out numero))
+    {
+        if (numero > 0)
+        {
+            Console.WriteLine("O NÚMERO É POSITIVO!!!");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("O NÚMERO É NEGATIVO!!!");
+        }
+        else
+        {
+            Console.WriteLine("ZERO É NULO");
+        }
+    }
+    else
+    { while (true)
+        {
+            Console.WriteLine("Entrada inválida.");
+            Console.WriteLine("Quer digitar outro número? ('Sim' ou 'Não')");
+            condicao = Console.ReadLine();
+            if (condicao == "Sim" || condicao == "sim")
+            { break; }
+            else if (condicao == "Não" || condicao == "nao" || condicao == "Nao" || condicao == "não")
+            { return; }
+            else { Console.WriteLine("Por favor, digite apenas 'Sim' ou 'Não'."); }
+        }
+    }
+}
