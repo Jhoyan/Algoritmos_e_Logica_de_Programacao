2c0c394c404511ee0337d67e17b5954515ed24d9
Jhoyan Macedo <jhoyanpdv@hotmail.com>
2025-04-10 12:49:37 -0400

    feat: introduction to lambda expressions
    Here we have a console application demonstrating lambda expressions in C#. The first exercise formats a list of products (name and price) into strings with currency formatting. The second exercise filters employees earning over R$3000, sorts them alphabetically by name, and formats their details. Both results are printed to the console.

---
+++ IntroductionToLambda/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToLambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 2: Crie uma lista de nomes de produtos e, usando expressões lambda, transforme essa lista em uma lista de strings que exibe o nome do produto e o preço formatado.
+            List<(string productName, decimal price)> products = new List<(string, decimal)> { ("Apple", 1m), ("Smartphone", 800m), ("Mouse", 100m), ("Sword", 200m) };
+            List<string> productsFormated = products.Select(x => $"\nProduct: {x.productName} - Price: {x.price.ToString("C2")}").ToList();
+
+            Console.WriteLine(string.Join(", ", productsFormated));
+
+
+            //Exercicio 3: Dada uma lista de funcionários, onde cada funcionário tem um nome e um salário, filtre os funcionários que ganham mais de R$ 3000, e em seguida, ordene a lista por nome.
+
+            List<(string name, double salary)> employee = new List<(string, double)> { ("Jorge", 2500), ("Jhoyan", 4201), ("Diogo", 4020), ("Guilherme", 4200) };
+            List<string> employeeOrdered = employee.Where(x => x.salary > 3000).OrderBy(x => x.name).Select(x => $"\nFuncionário: {x.name} - Salário {x.salary.ToString("C2")}").ToList();
+
+            Console.WriteLine(string.Join(", ", employeeOrdered));
+
+
+            Console.ReadKey();
+        }
+    }
+}
